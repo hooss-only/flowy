08ff83058cde66257ceaabade7a043e6c2d110d1
hooss-only <hoosshusu@gmail.com>
2023-08-12 19:21:21 +0900

    ✨ title in splashpage

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,13 +1,37 @@
+import 'package:flowy/widgets/gradient_text.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
+  final Gradient signitureGradient = const LinearGradient(
+    colors: [
+      Color(0xFF58BC46),
+      Color(0xFFBBD66C),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text(
+              "FLOWY",
+              style: TextStyle(
+                fontSize: 58,
+                fontFamily: "Pretendard",
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
