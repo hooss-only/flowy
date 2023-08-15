024eff3fece968e261dee839871001e420b31e5c
hooss-only <hoosshusu@gmial.com>
2023-08-15 19:32:56 +0900

    feat: sign up complete

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,17 @@
+import 'package:flowy/screens/signup/signup1_screen.dart';
 import 'package:flowy/widgets/gradient_text.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late SharedPreferences preferences;
   final Gradient signitureGradient = const LinearGradient(
     colors: [
       Color(0xFF58BC46),
@@ -12,6 +20,38 @@ class SplashScreen extends StatelessWidget {
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
+
+  Future<void> initPref() async {
+    preferences = await SharedPreferences.getInstance();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initPref();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      bool? signup = preferences.getBool('sign up');
+
+      if (!preferences.containsKey('sign up')) {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => (const Signup1Screen())));
+      }
+
+      if (signup != null) {
+        if (!signup) {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => (const Signup1Screen())));
+        }
+      } else {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => (const Signup1Screen())));
+      }
+
+      Navigator.pop(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
