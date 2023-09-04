093b00c720b02a091ff337fddd594ae576dc9676
hooss-only <hoosshusu@gmial.com>
2023-09-05 00:59:27 +0900

    fix: bug fixed about calling value

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -4,8 +4,8 @@ class WeatherModel {
 	final int humidity;
 
 	WeatherModel.fromJson(Map<String, dynamic> json)
-			: main = json['weather']['main'],
-				description = json['weather']['description'],
+			: main = json['weather'][0]['main'],
+				description = json['weather'][0]['description'],
 				temp = json['main']['temp'],
 				feels_like = json['main']['feels_like'],
 				humidity = json['main']['humidity'];
